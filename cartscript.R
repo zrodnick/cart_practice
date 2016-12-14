55c56f8b4181909e931f17c75208b5413df476da
zrodnick <zrodnick@gmail.com>
2016-12-14 17:57:19 -0600

    Added in random forest and cross validation stuff

--- cartscript.R
+++ cartscript.R
@@ -16,3 +16,23 @@ PredictROC <- predict(StevensTree, newdata=Test)
 pred <- prediction(PredictROC[,2], Test$Reverse)
 perf <- performance(pred, "tpr", "fpr")
 plot(perf)
+
+library(randomForest)
+
+Train$Reverse = as.factor(Train$Reverse)
+Test$Reverse = as.factor(Test$Reverse)
+
+StevensForest <- randomForest(Reverse ~ Circuit+Issue+Petitioner+Respondent+LowerCourt+Unconst, data=Train, nodesize=25, ntree=200)
+PredictForest <- predict(StevensForest, newdata=Test)
+table(Test$Reverse, PredictForest)
+library(caret)
+library(e1071)
+
+fitControl <- trainControl(method="cv", number=10)
+cartGrid <- expand.grid(.cp=(1:50)*0.01)
+train(Reverse ~ Circuit+Issue+Petitioner+Respondent+LowerCourt+Unconst, data=Train, method="rpart", trControl=fitControl, tuneGrid=cartGrid)
+
+StevensTreeCV <- rpart(Reverse ~ Circuit+Issue+Petitioner+Respondent+LowerCourt+Unconst, method="class", data=Train, control=rpart.control(cp=0.17))
+PredictCV=predict(StevensTreeCV, newdata=Test, type="class")
+
+table(Test$Reverse, PredictCV)
